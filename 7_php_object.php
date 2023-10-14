018a856551b512428ef33be17512a56d1834fb92
imran <zunaeede@gmail.com>
2023-10-14 23:39:49 +0600

    main - php object inharit

---
+++ 7_php_object.php
@@ -0,0 +1,30 @@
+<?php
+
+class car{
+  public $color;
+  public $model;
+  public function __construct($color, $model){
+        $this->color = $color;
+        $this->model = $model;
+     }
+     public function massege(){
+      return "My car is a " .  $this->color . " " . $this->model;
+     }
+    }
+
+
+    
+   $myCar = new Car("Black", "volvo");
+   echo $myCar-> massege();
+   echo "<br>"; 
+
+   $myCar = new car("red", "toyota");
+   echo $myCar-> massege();
+   echo "<br>"; 
+
+   $myCar = new car("Green", "GreenLine");
+   echo $myCar-> massege();
+
+
+
+?>
