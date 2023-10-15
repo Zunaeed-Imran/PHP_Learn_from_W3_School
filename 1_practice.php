9f7ebd3e530fde9f56a4b112f6620af19e9011fc
imran <zunaeede@gmail.com>
2023-10-15 11:32:24 +0600

    practice - practice object

--- 1_practice.php
+++ 1_practice.php
@@ -1,5 +1,24 @@
 <?php
-$x = null;
-$x = "Hello world!";
-var_dump($x);
+
+class itlogiko{
+  protected $work;
+  protected $time;
+  protected $rule;
+
+  function __construct($work, $time, $rule){
+      $this->work = $work;
+      $this->time = $time;
+      $this->rule = $rule;
+     }
+     public function massege(){
+        return "Our office" . $this->work . " " . $this->time . " " . $this->rule;
+      }
+    }
+
+
+$officeCode = new car("Software", "9-6", "just concreat and work");
+echo $office->massege();
+
+
+
 ?>
