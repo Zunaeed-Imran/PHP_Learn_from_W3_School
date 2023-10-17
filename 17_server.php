8c2454a3a9173161fdf840a9e6057c08bb28272b
imran <zunaeede@gmail.com>
2023-10-17 19:00:41 +0600

    main - server address

--- 17_server.php
+++ 17_server.php
@@ -5,6 +5,7 @@
 echo $_SERVER['PHP_SELF'] ."<br>";
 echo $_SERVER['HTTP_HOST'] ."<br>";
 echo $_SERVER['SCRIPT_NAME'] ."<br>";
+echo $_SERVER['SERVER_ADDR'] ."<br>";
 echo $_SERVER['SERVER_NAME'];
 
 
