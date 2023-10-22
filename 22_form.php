e8c158298ea073ca831778a9daaf9ceab4ca0bc9
imran <zunaeede@gmail.com>
2023-10-22 18:08:45 +0600

    main - trying to find the problem on tha form

--- 22_form.php
+++ 22_form.php
@@ -17,9 +17,41 @@
         $nameError = 'Name required';
       }else{
         $name = test($_POST['name']);
-        if(!preg_match())
+        if(!preg_match("/^[a-zA-Z-' ]*$/",$name)){
+          $nameError = "Only letter and space allowed";
+        }
+      }
+      if(empty($_POST["email"])){
+        $emailError = "Email Required";
+      } else {
+        $email = test($_POST['email']);
+        if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+          $emailError = 'Invalid email format';
+        }
+      }
+      if(empty($_POST['website'])){
+        $website = '';
+      }else{
+        $website = test($_POST['website']);
+        if(!preg_match("/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i", $website)){
+          $websiteError = "Invalid URL";
+        }
+      }
+      if (empty($_POST['comment'])){
+        $comment = '';
+      }else{
+        $comment = test($_POST['comment']);
+      }
+      if(empty($_POST['gender'])){
+        $genderError = "Gender Required";
+      } else {
+        $gender = test($_POST['gender']);
       }
     }
+
+  function test($data){
+    return $data;
+  }
 
   ?>
 
@@ -37,11 +69,25 @@
     <br>
     Comment: <textarea name="comment" cols="30" rows="5"><?php echo $comment ?></textarea>
     <br>
+
+
+
+Gender:
+<input type="radio" name="gender" <?php if(isset($gender) && $gender == "female") echo "checked"; ?> value="female">Female
+<input type="radio" name="gender" <?php if(isset($gender) && $gender == "male") echo "checked"; ?> value="male">Male
+<input type="radio" name="gender" <?php if(isset($gender) && $gender == "other") echo "checked"; ?> value="other">Other
+<span class="error">*<?php echo $genderError; ?></span>
+
+
+
+
     Gender: 
-    <input type="ratio" name="gender" <?php if(isset($gender) && $gender == "male") echo "checked"; ?> value="male">Male
     <input type="ratio" name="gender" <?php if(isset($gender) && $gender == "female") echo "checked"; ?> value="female">Female
+    <input type="ratio" name="gender" <?php if(isset($gender) && $gender == "male") echo "checked"; ?> value="male">Male
     <input type="radio" name="gender" <?php if(isset($gender) && $gender == "other") echo "checked"; ?> value="other">Other
-    <span class="error"><?php echo $genderError ?></span>
+    <span class="error">*<?php echo $genderError ?></span>
+
+    
     <br>
     <input type="submit">
 
