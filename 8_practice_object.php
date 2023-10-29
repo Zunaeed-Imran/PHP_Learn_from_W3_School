4fc376afed3c44c4af17b9a3b29b604ae27ff6ea
imran <zunaeede@gmail.com>
2023-10-29 12:51:48 +0600

    practice - another OPP in PHP

--- 8_practice_object.php
+++ 8_practice_object.php
@@ -1,21 +1,13 @@
 <?php
 
-class stuff{
-  public $model;
-  public $name;
-
-  public function __construct($model, $name)  {
-    $this->model = $model;
-    $this->name = $name;
-  }
-
-    public function massege(){
-      return "My phone Model is " . $this->model . " and name is " . $this->name;
-    }
+class koli{
+  public $x;
 }
 
-$myStuff = new stuff("N9", "Nokia");
-echo $myStuff-> massege();
+$a = new koli();
+$a->x = "Virat";
+
+echo $a->x;
 
 
 ?>
