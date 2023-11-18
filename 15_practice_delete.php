a0b9330bc3fae65ca95f1c7cd63af6bacf0a4d5d
imran <zunaeede@gmail.com>
2023-11-18 20:15:20 +0600

    practice - delete practice

--- 15_practice_delete.php
+++ 15_practice_delete.php
@@ -1,7 +1,7 @@
 <?php
 include '15_practice_connect.php';
 
-if($connect->query("DELETE from `practice3` WHERE `id` = {$_POST['id']}")){
+if($connect->query("DELETE from `practice3` WHERE `id` = {$_REQUEST['id']}")){
   header('location:15_practice_crud_index.php');
 }else{
   die(mysqli_error($connect));
